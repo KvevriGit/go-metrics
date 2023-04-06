3d0887ef2edb9408cf4bf2349d783a29f23aded4
yui <yuiop@air.local>
2023-04-06 16:21:30 +0300

    test2

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+	"strings"
+)
+
+type MemStorage struct {
+	strg []string
+}
+
+func mainPage(w http.ResponseWriter, r *http.Request) {
+	rawUrl := r.URL.String()
+	splitURL := strings.Split(rawUrl, "/")
+}
+
+func main() {
+	http.ListenAndServe("localhost:8080", nil)
+	mx := http.NewServeMux()
+	mx.HandleFunc("/", mainPage)
+}
